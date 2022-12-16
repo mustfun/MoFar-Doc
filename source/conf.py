7a76f1380286cd7a264828501a8fa707d360ccb1
itar.deng <itar.deng@ximalaya.com>
2022-12-16 19:54:14 +0800

    update doc

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.36'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = ['recommonmark']
 
 templates_path = ['_templates']
 exclude_patterns = []
