6b47274b27ea65d695c1eca539532213d7d89374
itar.deng <itar.deng@ximalaya.com>
2022-12-16 19:35:20 +0800

    first commit

---
+++ source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MoFar'
+copyright = '2022, itar.deng'
+author = 'itar.deng'
+release = '1.0.36'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['recommonmark','sphinx_markdown_tables']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+source_suffix = ['.rst', '.md']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# 主题doc https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+}
+html_static_path = ['_static']
+
+
+# Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr', 'zh'
+html_search_language = 'zh'
+
+
+# A dictionary with options for the search language support, empty by default.
+# 'ja' uses this config value.
+# 'zh' user can custom change `jieba` dictionary path.
+# html_search_options = {'dict': '/usr/lib/jieba.txt'}   # 根据需要设置jieba的词典路径
---
+++ source/index.rst
@@ -0,0 +1,33 @@
+.. MoFar documentation master file, created by
+   sphinx-quickstart on Thu Dec 15 15:57:18 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+MoFar 1.0.36
+=================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: 简介:
+   :glob:
+
+   intro/*
+
+.. toctree::
+   :maxdepth: 3
+   :caption: 使用:
+   :glob:
+
+   use/*   
+
+.. toctree::
+   :maxdepth: 3
+   :caption: QA:
+   :glob:
+
+   qa/*   
+
+
+
+如有疑问欢迎到QA页联系我~
+==================
